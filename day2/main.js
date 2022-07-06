ebcb6a5d88158fd6bb797251f7669245ef0862c1
Mateus Francisco dos Santos Silva <99891834+mateuscsco@users.noreply.github.com>
2022-07-06 12:45:45 -0300

    implementação do js

--- day2/main.js
+++ day2/main.js
@@ -1,2 +1,20 @@
-alert('olá mundo')
-document.querySelector(input);
+const form = document.getElementById("form");
+const nome = document.getElementById("nome");
+const idade = document.getElementById("idade");
+const linguagem = document.getElementById("linguagem");
+if(form){
+    el.addEventListener('click', swapper, false);
+  }
+onclick(enviar){
+    alert("Olá " +nome+"você tem "+idade+ " anos e já está aprendendo "+linguagem+ "!")
+}
+//form.addEventListener("enviar", (e)=>{
+//    e.preventDefault();
+//});
+
+//function checkInputs(){
+//    const nomeValue = nome.value;
+//    const idadeValue = idade.value;
+//    const linguagemValue = linguagem.value;
+//}
+
